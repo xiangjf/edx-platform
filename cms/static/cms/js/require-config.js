78f96fe0814ccb67f54564a96ac45ec0e1f5c44d
Chris Rodriguez <github@chrisrodriguez.me>
2016-02-09 12:00:12 -0500

    Cleanup

--- cms/static/cms/js/require-config.js
+++ cms/static/cms/js/require-config.js
@@ -14,7 +14,7 @@
         // Since we are serving the gettext catalog as static files,
         // the URL for the gettext file will vary depending on which locale
         // needs to be served. To handle this, we load the correct file in the
-        // rendered template and then use this to ensure that RequireJS knows
+        // rendered template and then use this to ensure that RequireJS knows 
         // how to find it.
         define("gettext", function () { return window.gettext; });
     }
@@ -67,6 +67,7 @@
             "URI": "js/vendor/URI.min",
             "ieshim": "js/src/ie_shim",
             "tooltip_manager": "js/src/tooltip_manager",
+            "draggabilly": "js/vendor/draggabilly.pkgd",
 
             // Files needed for Annotations feature
             "annotator": "js/vendor/ova/annotator-full",
@@ -289,16 +290,16 @@
             },
             "ova":{
                 exports: "ova",
-                deps: ["annotator", "annotator-harvardx", "video.dev", "vjs.youtube",
-                       "rangeslider", "share-annotator", "richText-annotator", "reply-annotator",
-                       "tags-annotator", "flagging-annotator", "grouping-annotator", "diacritic-annotator",
+                deps: ["annotator", "annotator-harvardx", "video.dev", "vjs.youtube", 
+                       "rangeslider", "share-annotator", "richText-annotator", "reply-annotator", 
+                       "tags-annotator", "flagging-annotator", "grouping-annotator", "diacritic-annotator", 
                        "jquery-Watch", "catch", "handlebars", "URI"]
             },
             "osda":{
                 exports: "osda",
-                deps: ["annotator", "annotator-harvardx", "video.dev", "vjs.youtube",
-                       "rangeslider", "share-annotator", "richText-annotator", "reply-annotator",
-                       "tags-annotator", "flagging-annotator", "grouping-annotator", "diacritic-annotator",
+                deps: ["annotator", "annotator-harvardx", "video.dev", "vjs.youtube", 
+                       "rangeslider", "share-annotator", "richText-annotator", "reply-annotator", 
+                       "tags-annotator", "flagging-annotator", "grouping-annotator", "diacritic-annotator", 
                        "openseadragon", "jquery-Watch", "catch", "handlebars", "URI"]
             },
             "draggabilly": {
