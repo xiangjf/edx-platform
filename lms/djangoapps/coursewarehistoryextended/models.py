a2129d372e3de94cdff49341fca16cff392a402e
Kevin Falcone <kevin@edx.org>
2016-02-18 17:15:11 -0500

    Implement a BaseStudentModuleHistory
    This abstract class contains most of the fields (Aside from the id and
    foreign key to StudentModule that the subclasses need to manage).  It
    also provides a get_history method that abstracts searching across
    multiple backends.
    
    Update tests to explicitly use this get_history method rather than
    looking directly into StudentModuleHistory or
    StudentModuleHistoryExtended.

--- lms/djangoapps/coursewarehistoryextended/models.py
+++ lms/djangoapps/coursewarehistoryextended/models.py
@@ -3,31 +3,23 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver, Signal
 
 from coursewarehistoryextended.fields import UnsignedBigIntAutoField
-from courseware.models import StudentModule, ChunkingManager
+from courseware.models import StudentModule, BaseStudentModuleHistory
 
-class StudentModuleHistoryExtended(models.Model):
+class StudentModuleHistoryExtended(BaseStudentModuleHistory):
     """Keeps a complete history of state changes for a given XModule for a given
     Student. Right now, we restrict this to problems so that the table doesn't
     explode in size.
 
     This new extended CSMH has a larger primary key that won't run out of space
     so quickly."""
-    objects = ChunkingManager()
-    HISTORY_SAVING_TYPES = {'problem'}
 
     class Meta(object):
+        app_label = 'coursewarehistoryextended'
         get_latest_by = "created"
 
     id = UnsignedBigIntAutoField(primary_key=True)  # pylint: disable=invalid-name
 
     student_module = models.ForeignKey(StudentModule, db_index=True, db_constraint=False, on_delete=models.DO_NOTHING)
-    version = models.CharField(max_length=255, null=True, blank=True, db_index=True)
-
-    # This should be populated from the modified field in StudentModule
-    created = models.DateTimeField(db_index=True)
-    state = models.TextField(null=True, blank=True)
-    grade = models.FloatField(null=True, blank=True)
-    max_grade = models.FloatField(null=True, blank=True)
 
     @receiver(post_save, sender=StudentModule)
     def save_history(sender, instance, **kwargs):  # pylint: disable=no-self-argument, unused-argument
@@ -55,14 +47,3 @@ class StudentModuleHistoryExtended(models.Model):
 
     def __unicode__(self):
         return unicode(repr(self))
-
-    @property
-    def csm(self):
-        """
-        Finds the StudentModule object for this history record, even if our data is split
-        across multiple data stores.  Django does not handle this correctly with the built-in
-        student_module property.
-        """
-        return StudentModule.objects.filter(pk=self.student_module_id).first()
-
-
